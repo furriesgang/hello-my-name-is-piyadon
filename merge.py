5c7738e92ce38eacd88cc8dffb508a9bad936adb
blue <canikissfemboy@gmail.com>
2024-10-22 12:24:32 +0700

    I HATE MIZUKI FUCK

--- merge.py
+++ merge.py
@@ -7,12 +7,13 @@ json_files = ['blueskychan_.json', 'bluestar.json', 'furrygang.json', 'mizuki403
 merged_data = []
 
 for json_file in json_files:
+    print(json_file)
     file_path = os.path.join(input_dir, json_file)
     with open(file_path, 'r') as f:
         data = json.load(f)
         merged_data.append(data)
 
 with open(output_file, 'w') as f:
-    json.dump(merged_data, f, indent=4, ensure_ascii=True)
+    json.dump(merged_data, f, indent=4, ensure_ascii=False)
 
 print(f'Merged data written to {output_file}')
