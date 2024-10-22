d86126d7f4e8fa2924653ef397cf85b849b93266
blue <canikissfemboy@gmail.com>
2024-10-22 12:15:47 +0700

    skill issues 5

--- merge.py
+++ merge.py
@@ -3,7 +3,7 @@ import os
 
 input_dir = 'data/peoples/'
 output_file = 'data/peoples.json'
-json_files = ['blueskychan_.json', 'bluestar.json', 'furrygang.json', 'mizuki403.json']
+json_files = ['blueskychan_.json', 'bluestar.json', 'furrygang.json', 'mizuki403_.json']
 merged_data = []
 
 for json_file in json_files:
